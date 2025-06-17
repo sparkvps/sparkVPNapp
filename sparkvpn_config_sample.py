2ca4736c3fff9aff2686293eed19f732e9f72aed
sparkvps <158144731+sparkvps@users.noreply.github.com>
2025-06-17 17:44:46 +0330

    Delete sparkvpn_config_sample.py

--- sparkvpn_config_sample.py
+++
@@ -1,9 +0,0 @@
-# SparkVPN Configuration
-USERNAME = 'amir'
-PASSWORD = 'sparkvpn'
-static_configs = [
-  'vless://c521e9d8...موقتی',
-  'vless://c521e9d8...vision',
-  'vless://9b915198...tcp#Sparkvpn',
-  'vless://c521e9d8...فرانسه'
-]
